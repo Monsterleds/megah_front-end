16292ddbf723613b39c6721bfa8b4f3edb41e7ba
unknown <kha.led002@hotmail.com>
2020-05-04 15:04:55 -0300

    fix: some bugs

--- src/pages/CreateCompany/index.js
+++ src/pages/CreateCompany/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
 
@@ -104,51 +103,3 @@ export default function CreateCompany() {
     </Wrapped>
   );
 }
-=======
-import React from 'react';
-
-import { Container, Wrapped, Nav } from './styles';
-import {Form, Input} from '@rocketseat/unform';
-import {Link} from 'react-router-dom';
-import logo from '../../assets/gripplogo.png';
-
-
-
-class CreateCompany extends React.Component{
-    render(){
-        function handleSubmit(data) {
-            console.log(data);
-          }
-  return (
-
-    <Wrapped>
-        <Nav>
-            <Link id="logo"><img alt="Logo" src={logo} /></Link>
-                <ul>
-                <Link>Inicio</Link>
-                <Link>Projetos</Link>
-                <Link>
-                    {'username'}
-                <img id="avatar" alt="Avatar" src={'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2017/03/Avatar-1280x720.jpg'} />
-                </Link>
-                </ul>
-        </Nav>
-        
-    <Container>
-        <Form onSubmit={handleSubmit}>
-            <h1>Crie sua marca</h1>
-            <Input name="name" type="text" placeholder="Nome da Marca" />
-            <Input name="description" type="text" placeholder="Descrição da Marca" />
-            <Input name="img_url" type="file"/>
-            <input id="btn" className="input" type="submit" />
-        </Form>
-        
-    </Container>
-    
-    </Wrapped>
-  )
-    }
-}
-
-export default CreateCompany;
->>>>>>> ee15df13ccaeae07aff0def25fa7f79bd02a7e09
--- src/pages/CreateCompany/styles.js
+++ src/pages/CreateCompany/styles.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled, { css } from 'styled-components';
 import backgroundImage from '../../assets/createCompanyBG.svg';
 
@@ -35,39 +34,6 @@ export const Nav = styled.div`
         font-family: roboto, bold;
     }
     img{
-=======
-import styled from 'styled-components';
-import backgroundImage from  '../../assets/createCompanyBG.svg'
-import pencil from "../../assets/pencil.svg";
-
-
-
-
-export const Nav = styled.div`
-width: 99vw;
-display:flex;
-justify-content: space-between;
-align-items: center;
-    #logo{
-        margin: 0% 3%;
-
-    }
-    ul{
-        display:flex;
-        
-        align-items: center;
-    }
-    a{
-        display:flex;
-        justify-content: center;
-        align-items: center;
-        text-decoration: none;
-        margin: 5px;
-        color:white;
-        font-family: roboto, bold;
-    }
-    #avatar{
->>>>>>> ee15df13ccaeae07aff0def25fa7f79bd02a7e09
         width: 50px;
         height: 50px;
         border-radius: 50%;
@@ -77,7 +43,6 @@ align-items: center;
 `;
 
 export const Wrapped = styled.div`
-<<<<<<< HEAD
     height: 100vh;  
     background-image: url(${backgroundImage});
     background-size: 85%;
@@ -98,47 +63,12 @@ export const Container = styled.div`
             padding: 15px 0px;
         }
         width: 400px;
-=======
-height: 100vh;
-    
-background-image: url(${backgroundImage});
-    background-size: 85%;
-    background-position: right top;
-    background-repeat: no-repeat;
-    
-    
-`
-
-export const Container = styled.div`
-    
-    
-    opacity: 1;
-    
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height:82.7vh;
-
-
-    form{
-        h1{
-            font-size: 24px;
-            color: rgba(0,0,20,0.8);
-            padding: 15px 0px;
-            
-
-        }
-        #img{
-            padding:  10px;
-        }
->>>>>>> ee15df13ccaeae07aff0def25fa7f79bd02a7e09
         display: flex;
         flex-direction: column;
         align-items: center;
         background: rgb(250,250,250);
         padding: 30px 20px;
         border-radius: 5px;
-<<<<<<< HEAD
         
         input{
             width: 90%;
@@ -161,34 +91,10 @@ export const Container = styled.div`
             display: flex;
             flex-direction: row;
             justify-content: flex-start;
-=======
-        #btn{
-            
-            background: transparent linear-gradient(135deg, #50D1F1 0%, #535CEB 100%) 0% 0% no-repeat padding-box;
-            box-shadow: none;
-            color: #FFFFFF;
-            
-            }
-        input{
-            
-            padding: 10px 100px 10px 50px;
-            margin: 8px 0px;
-            border:none;
-            border-radius: 5px;
-            background: #FFFFFF 0% 0% no-repeat padding-box;
-            background-image: url(${pencil});
-            background-size: 30px;
-            background-position: 10px;
-            box-shadow: 0px 3px 6px #51ACEF;
-            opacity: 1;
-            
-            
->>>>>>> ee15df13ccaeae07aff0def25fa7f79bd02a7e09
         }
     }
 `;
 
-<<<<<<< HEAD
 export const SubmitButton = styled.button.attrs({
   type: 'submit',
 })`
@@ -244,6 +150,3 @@ export const InputFile = styled.input`
     position: absolute;
     opacity: 0%;
 `;
-=======
-
->>>>>>> ee15df13ccaeae07aff0def25fa7f79bd02a7e09
